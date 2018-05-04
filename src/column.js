83ddb21a9f8f8b109afab520d4f03c35f0c4db34
a-lizards-breath <elizabeth.wesa@trunkclub.com>
2018-05-04 15:46:57 -0500

    removed log

--- src/column.js
+++ src/column.js
@@ -3,7 +3,6 @@ import Box from './box'
 
 class Column extends React.Component {
   render() {
-    console.log(this.props.boxes)
     return (
       <div className={this.props.className}>
         {this.props.boxes.map( box => <Box color={box.color}/>)}
