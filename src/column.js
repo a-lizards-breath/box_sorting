3150f50def4b6e6904de9b508514ed28c97a24f1
a-lizards-breath <elizabeth.wesa@trunkclub.com>
2018-05-04 15:46:36 -0500

    columns can take arrays of boxes

--- src/column.js
+++ src/column.js
@@ -3,10 +3,10 @@ import Box from './box'
 
 class Column extends React.Component {
   render() {
+    console.log(this.props.boxes)
     return (
       <div className={this.props.className}>
-        <Box color="blue"/> 
-        <Box color="red"/>
+        {this.props.boxes.map( box => <Box color={box.color}/>)}
       </div>
     )
   }
--- src/page.js
+++ src/page.js
@@ -3,11 +3,14 @@ import Column from './column'
 
 class Page extends React.Component {
   render() {
+    let left   = [{color: "red"}, {color: "blue"}] 
+    let center = [{color: "blue"}, {color: "blue"}] 
+    let right  = [{color: "red"}] 
     return (
       <div className="page">
-        <Column className="column-left" />
-        <Column className="column-center" />
-        <Column className="column-right" />
+        <Column className="column-left" boxes={left}/>
+        <Column className="column-center" boxes={center}/>
+        <Column className="column-right" boxes={right}/>
       </div>
     )
   }
